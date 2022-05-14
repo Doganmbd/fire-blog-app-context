28b195c6ec86ee5942d683c3a9de2a475e600462
DMB29 <mbilaldogan90@gmail.com>
2022-05-14 19:42:44 +0300

    create AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,10 @@
-import {createContext , useContext } from 'react';
+import {createContext  } from 'react';
 
 
 const AuthContext = createContext();
 
 
-import React from 'react'
+
 
 const AuthContextProvider = ({children}) => {
 
