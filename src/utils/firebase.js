01ec78157403cf58fa0a356fbe5556172a2b04c6
DMB29 <mbilaldogan90@gmail.com>
2022-05-21 00:52:59 +0300

    signInWithEmailAndPassword

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth ,createUserWithEmailAndPassword} from "firebase/auth";
+import { getAuth ,createUserWithEmailAndPassword,signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 
 // Your web app's Firebase configuration
@@ -19,7 +19,7 @@ const auth = getAuth(app);
 
 // register sayfasından email ve passwordu çekmek için 
 
-export const createUser = async(email,password)=> {
+export const createUser = async(email,password,displayName)=> {
     try {
         await
         createUserWithEmailAndPassword(auth, email, password)
@@ -27,7 +27,17 @@ export const createUser = async(email,password)=> {
             displayName: displayName, photoURL: ""
           });
         }
-          catch(error) => {
+          catch(error){
             alert(error.message)
           }
 }
+
+
+export const signIn = async (email, password, navigate) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+   
+  } catch (err) {
+    alert(err.message);
+  }
+};
