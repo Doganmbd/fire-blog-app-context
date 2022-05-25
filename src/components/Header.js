3d4a0b9043d57663d93eaa0dbc7eadf20ee4d5b8
DMB29 <mbilaldogan90@gmail.com>
2022-05-25 19:44:59 +0300

    dashboard header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1> M.B.D -------- M.B.D -------- M.B.D --------M.B.D --------M.B.D  </h1>
+    </div>
+  )
+}
+
+export default Header
