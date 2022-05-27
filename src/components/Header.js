741e10d272e40bedbf1ccf3da86f9d8976d51f27
DMB29 <mbilaldogan90@gmail.com>
2022-05-27 16:59:24 +0300

    dashboard , blogcard , header create

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "./style.css"
 const Header = () => {
   return (
     <div className="bounce">
-        <h1> M.B.D -------- M.B.D -------- M.B.D --------M.B.D --------M.B.D  </h1>
+        <h1> M.B.D -------- M.B.D -------- M.B.D  </h1>
     </div>
   )
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "../components/Header"
+import BlogCard from "../components/BlogCard"
 
 const Dashboard = () => {
   return (
     <div>
         <Header />
+        <BlogCard />
     </div>
   )
 }
