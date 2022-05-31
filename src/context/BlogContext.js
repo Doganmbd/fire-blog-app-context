e0137453a8b49d8e99c600be97aad12083267058
DMB29 <mbilaldogan90@gmail.com>
2022-05-31 09:06:20 +0300

    blogcontext complete

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,26 @@
-import  {createContext,useContext} from "react";
+import  {createContext,useContext,useState} from "react";
 
 
 export const BlogContext = createContext();
 
+const initialValues = {
+    title:"",
+    imageUrl:"",
+    content:"",
+    date:""
+}
 
 const BlogContextProvider = ({children}) => {
 
+    const [data, setData] = useState(initialValues);
 
-
+    const date = new Date().toLocaleDateString() + " "
+    const time = new Date().toLocaleTimeString().slice(0,5)
 
 
 
     return (
-        <BlogContext.Provider >
+        <BlogContext.Provider value={{data,date,time,setData}} >
             {children}
         </BlogContext.Provider>
     )
