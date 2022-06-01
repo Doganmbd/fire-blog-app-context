3e18cf3255130be0789020146809826dc3db4fd2
DMB29 <mbilaldogan90@gmail.com>
2022-06-01 15:10:23 +0300

    like button

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import {createContext ,useState ,useEffect,useContext} from 'react';
-import {userObserver} from "../utils/firebase"
+import {EditUser, userObserver} from "../utils/firebase"
 
 
 export const AuthContext = createContext();
@@ -12,6 +12,27 @@ const AuthContextProvider = ({children}) => {
   const [currentUser, setCurrentUser] = useState()
 
 
+  const handleFavIcon = (e, data) => {
+    e.stopPropagation();
+    // console.log(data)
+    if (!currentUser) {
+      // toastWarnNotify("please login to like")
+    } else {
+        if (data.likedUserIds) {
+          if (data.likedUserIds.includes(currentUser.uid)) {
+            EditUser({ ...data, likedUserIds: data.likedUserIds.filter((item) => !(item === currentUser.uid)) })
+            data.likedUserIds.filter((item) => !(item === currentUser.uid))
+          } else {
+            data.likedUserIds.push(currentUser.uid)
+            EditUser({ ...data, likedUserIds: data.likedUserIds })
+          }
+        } else {
+          EditUser({ ...data, likedUserIds: currentUser.uid.split(" ") })
+        }
+    }
+  }
+
+
 
   useEffect(() => {
     //usser signin signout olduğunda takip eden fonk.
@@ -20,7 +41,7 @@ const AuthContextProvider = ({children}) => {
 
 
   return (
-    <AuthContext.Provider value={{currentUser}}>
+    <AuthContext.Provider value={{currentUser,handleFavIcon}}>
         {children}
     </AuthContext.Provider>
   )
