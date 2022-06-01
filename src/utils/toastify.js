30e307498bc34715d3ddfaf9c35aaa74192f9b5d
DMB29 <mbilaldogan90@gmail.com>
2022-06-01 11:06:17 +0300

    tastify create

---
+++ src/utils/toastify.js
@@ -0,0 +1,27 @@
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+
+export const Toastify = (msg)=>{
+
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+}
+
+export const ToastifyInfo = (msg)=>{
+  toast.info(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+}
